875bc5b4598a1e70afabac7f16b80f1131ba486f
Dhruv Saini <dhruvsainims231@gmail.com>
2026-07-25 19:48:20 +0530

    Add solution for LeetCode 15 (3Sum)

---
+++ leetcode15.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> ans;
+        
+        
+        sort(nums.begin(), nums.end());
+        
+        
+        for (int i = 0; i < n - 2; i++) {
+          
+            if (nums[i] > 0) break;
+            
+            
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int j = i + 1;
+            int k = n - 1;
+            
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum == 0) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    
+                    
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+               
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    
+                    j++;
+                    k--;
+                } else if (sum < 0) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+int main() {
+    Solution solver;
+    vector<int> nums = {-1, 0, 1, 2, -1, -4};
+    
+    vector<vector<int>> result = solver.threeSum(nums);
+    
+    cout << "Triplets summing to 0:" << endl;
+    for (const auto& triplet : result) {
+        cout << "[" << triplet[0] << ", " << triplet[1] << ", " << triplet[2] << "]" << endl;
+    }
+    
+    return 0;
+}
