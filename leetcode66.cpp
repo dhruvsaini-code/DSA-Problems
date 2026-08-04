59ba4fb0033378627e65dfcaa4e9d43c995dd4b3
Dhruv Saini <dhruvsainims231@gmail.com>
2026-08-04 21:00:40 +0530

    Add solution for LeetCode 66: Plus One

---
+++ leetcode66.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        digits.insert(digits.begin(), 1);
+        return digits;
+    }
+};
+
+int main() {
+    Solution solver;
+    vector<int> digits = {1, 2, 3};
+    vector<int> result = solver.plusOne(digits);
+    
+    cout << "Result: ";
+    for (int d : result) {
+        cout << d << " ";
+    }
+    cout << endl;
+    return 0;
+}
